100bf7a846f0df9fe6409353ff791376c03da602
lavakush7800 <lavakushgiri@gmail.com>
2021-04-24 11:27:08 +0530

    19

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,9 +28,34 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-<div class="row">
-  <div class="col-12 col-md-8"><h2>About Us</h2></div>
-  <div class="col-6 col-md-4"><h2>About Us</h2></div>
-</div>
+  <div class="row">
+    <div class="col-12 col-md-8"><h2>About Us</h2></div>
+    <div class="col-6 col-md-4">
+    <div class="container-fluid card">
+      <div class="card-body"><h4>Contact Us</h4></div>
+          <div class="container-fluid">
+                <form class="col px-md-10" action="/action_page.php" style="margin-bottom: 25px;">
+                  <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" class="form-control" id="name" name="name">
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email address:</label>
+                    <input type="email" class="form-control" id="email">
+                  </div>
+                  <div class="form-group">
+                    <label for="phone">Phone Number:</label>
+                    <input type="text" class="form-control" id="phone" name="phone">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleFormControlTextarea4">Descriptions</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea4" rows="3"></textarea>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+          </div>
+    </div>  
+    </div>
+ </div>
 </div>
 @endsection
